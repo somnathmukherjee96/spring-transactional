d1a9d856bbb84a5a161754bd1c02c38df98482f4
Somnath Mukherjee <somnathmukherjee@Somnaths-MacBook-Pro.local>
2024-05-26 23:32:59 +0530

    initial setup

---
+++ src/test/java/com/som/transaction/SpringTransactionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.som.transaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTransactionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
